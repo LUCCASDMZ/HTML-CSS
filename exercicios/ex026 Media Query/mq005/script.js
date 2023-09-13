1e025105453b383b724cad620459206c9a2c1826
LUCCASDMZ <lucasduarte2317@gmail.com>
2023-09-13 00:17:46 -0300

    att

--- exercicios/ex026 Media Query/mq005/script.js
+++ exercicios/ex026 Media Query/mq005/script.js
@@ -6,3 +6,13 @@ function clickMenu(){
         itens.style.display = 'none'
     }
 }
+
+function mudouTamanho(){
+    const itens = document.getElementById('itens')
+    if (window.innerWidth >= 768){
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+
+}
