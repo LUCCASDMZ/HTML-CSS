ab402d36b7b2e20cb40d709a04b2a46fbc64c017
LUCCASDMZ <lucasduarte2317@gmail.com>
2023-09-12 23:50:42 -0300

    Delete script.js

--- exercicios/ex026 Media Query/mq005/script.js
+++
@@ -1,8 +0,0 @@
-function clickMenu(){
-    const itens = document.getElementById('itens')
-    if (itens.style.display == 'none') {
-        itens.style.display = 'block'
-    } else {
-        itens.style.display = 'none'
-    }
-}
