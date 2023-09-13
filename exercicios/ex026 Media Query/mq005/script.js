d168e2ec98f3ac78221c5feea7a0c4aee793c0d3
LUCCASDMZ <lucasduarte2317@gmail.com>
2023-09-12 23:52:55 -0300

    Create script.js

---
+++ exercicios/ex026 Media Query/mq005/script.js
@@ -0,0 +1,8 @@
+function clickMenu(){
+    const itens = document.getElementById('itens')
+    if (itens.style.display == 'none') {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
